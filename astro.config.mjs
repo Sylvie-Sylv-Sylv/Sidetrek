84be601e61e0b8f323a9a5a658c51bd4489b298e
SylvieSylvSylv <sylvie.sylv.sylv@gmail.com>
2026-08-23 22:39:01 +0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import UnoCSS from 'unocss/astro';
+
+export default defineConfig({
+  site: 'https://sylvie-sylv-sylv.github.io',
+  base: '/sidetrek',
+
+  integrations: [
+    mdx(),
+    react(),
+    UnoCSS(),
+  ],
+});
