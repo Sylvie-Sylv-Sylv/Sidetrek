9b36a381abb58ec9ae94162f0b3f2bc7c21d4daf
SylvieSylvSylv <sylvie.sylv.sylv@gmail.com>
2026-08-28 14:11:26 +0700

    value noise post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,26 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import UnoCSS from 'unocss/astro';
 
+import { unified } from '@astrojs/markdown-remark';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 export default defineConfig({
   site: 'https://sylvie-sylv-sylv.github.io',
   base: '/Sidetrek',
 
   integrations: [
-    mdx(),
+    mdx({
+      processor: unified({
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+      }),
+    }),
     react(),
     UnoCSS(),
   ],
